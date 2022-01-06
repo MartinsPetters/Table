e63d72a552d3b3c46baee0986927dc76019d8c12
Martins Petters <martins.petters@gmail.com>
2022-01-07 00:22:59 +0200

    part2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useTable, useExpanded, useSortBy, useFilters } from 'react-table';
+import {
+	useTable,
+	useExpanded,
+	useSortBy,
+	useFilters,
+	useResizeColumns,
+	useBlockLayout,
+	useColumnOrder,
+	useAbsoluteLayout,
+} from 'react-table';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import CircularJSON from 'circular-json';
 import ReactJson from 'react-json-view';
 
@@ -8,6 +18,48 @@ import makeData from './makeData';
 
 const Styles = styled.div`
 	padding: 1rem;
+
+	padding: 1rem;
+
+	* {
+		box-sizing: border-box;
+	}
+
+	.table {
+		border: 1px solid #000;
+		max-width: 700px;
+		overflow-x: auto;
+	}
+
+	.header {
+		font-weight: bold;
+	}
+
+	.rows {
+		overflow-y: auto;
+	}
+
+	.row {
+		border-bottom: 1px solid #000;
+		height: 32px;
+
+		&.body {
+			:last-child {
+				border: 0;
+			}
+		}
+	}
+
+	.cell {
+		height: 100%;
+		line-height: 31px;
+		border-right: 1px solid #000;
+		/* padding-left: 5px; */
+
+		:last-child {
+			border: 0;
+		}
+	}
 
 	table {
 		border-spacing: 0;
@@ -31,6 +83,23 @@ const Styles = styled.div`
 			:last-child {
 				border-right: 0;
 			}
+			.resizer {
+				display: inline-block;
+				background: blue;
+				width: 10px;
+				height: 100%;
+				position: absolute;
+				right: 0;
+				top: 0;
+				transform: translateX(50%);
+				z-index: 1;
+				${'' /* prevents from scrolling while dragging on touch devices */}
+				touch-action:none;
+
+				&.isResizing {
+					background: red;
+				}
+			}
 		}
 	}
 `;
@@ -46,6 +115,10 @@ function DefaultColumnFilter({ column: { filterValue, preFilteredRows, setFilter
 				setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
 			}}
 			placeholder={`Search ${count} records...`}
+			style={{
+				width: '100%',
+				boxSizing: 'border-box',
+			}}
 		/>
 	);
 }
@@ -132,38 +205,55 @@ function NumberRangeColumnFilter({ column: { filterValue = [], preFilteredRows, 
 		<div
 			style={{
 				display: 'flex',
+				flexDirection: 'row',
+				flexWrap: 'wrap',
 			}}
 		>
-			<input
-				value={filterValue[0] || ''}
-				type="number"
-				onChange={(e) => {
-					const val = e.target.value;
-					setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]]);
-				}}
-				placeholder={`Min (${min})`}
-				style={{
-					width: '70px',
-					marginRight: '0.5rem',
-				}}
-			/>
-			to
-			<input
-				value={filterValue[1] || ''}
-				type="number"
-				onChange={(e) => {
-					const val = e.target.value;
-					setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined]);
-				}}
-				placeholder={`Max (${max})`}
-				style={{
-					width: '70px',
-					marginLeft: '0.5rem',
-				}}
-			/>
+			<div>
+				<input
+					value={filterValue[0] || ''}
+					type="number"
+					onChange={(e) => {
+						const val = e.target.value;
+						setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]]);
+					}}
+					placeholder={`Min (${min})`}
+					style={{
+						width: '70px',
+						marginRight: '0.5rem',
+					}}
+				/>
+			</div>
+			<div>to</div>
+			<div>
+				<input
+					value={filterValue[1] || ''}
+					type="number"
+					onChange={(e) => {
+						const val = e.target.value;
+						setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined]);
+					}}
+					placeholder={`Max (${max})`}
+					style={{
+						width: '70px',
+						marginLeft: '0.5rem',
+					}}
+				/>
+			</div>
 		</div>
 	);
 }
+
+const IndeterminateCheckbox = React.forwardRef(({ indeterminate, ...rest }, ref) => {
+	const defaultRef = React.useRef();
+	const resolvedRef = ref || defaultRef;
+
+	React.useEffect(() => {
+		resolvedRef.current.indeterminate = indeterminate;
+	}, [resolvedRef, indeterminate]);
+
+	return <input type="checkbox" ref={resolvedRef} {...rest} />;
+});
 
 // Define a custom filter filter function!
 function filterGreaterThan(rows, id, filterValue) {
@@ -178,6 +268,20 @@ function filterGreaterThan(rows, id, filterValue) {
 // will be automatically removed. Normally this is just an undefined
 // check, but here, we want to remove the filter if it's not a number
 filterGreaterThan.autoRemove = (val) => typeof val !== 'number';
+
+const getItemStyle = ({ isDragging, isDropAnimating }, draggableStyle) => ({
+	...draggableStyle,
+	// some basic styles to make the items look a bit nicer
+	userSelect: 'none',
+
+	// change background colour if dragging
+	background: isDragging ? 'lightgreen' : 'grey',
+
+	...(!isDragging && { transform: 'translate(0,0)' }),
+	...(isDropAnimating && { transitionDuration: '0.001s' }),
+
+	// styles we need to apply on draggables
+});
 
 function Table({ columns: userColumns, data }) {
 	const filterTypes = React.useMemo(
@@ -216,13 +320,137 @@ function Table({ columns: userColumns, data }) {
 		},
 		useFilters, // filter hook
 		useSortBy, //sort hook
-		useExpanded // expand hook
+		useExpanded, // expand hook
+		useColumnOrder, //order
+		useAbsoluteLayout, //div table hook
+		useResizeColumns // resize hook
 	);
 
-	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = table;
+	const {
+		getTableProps,
+		getTableBodyProps,
+		getToggleHideAllColumnsProps,
+		headerGroups,
+		rows,
+		flatColumns,
+		setColumnOrder,
+		allColumns,
+		prepareRow,
+	} = table;
+
+	const currentColOrder = React.useRef();
 
 	return (
 		<>
+			<div {...getTableProps()} className="table">
+				<div>
+					{headerGroups.map((headerGroup) => (
+						<DragDropContext
+							onDragStart={() => {
+								currentColOrder.current = flatColumns.map((o) => o.id);
+							}}
+							onDragUpdate={(dragUpdateObj, b) => {
+								// console.log("onDragUpdate", dragUpdateObj, b);
+
+								const colOrder = [...currentColOrder.current];
+								const sIndex = dragUpdateObj.source.index;
+								const dIndex = dragUpdateObj.destination && dragUpdateObj.destination.index;
+
+								if (typeof sIndex === 'number' && typeof dIndex === 'number') {
+									colOrder.splice(sIndex, 1);
+									colOrder.splice(dIndex, 0, dragUpdateObj.draggableId);
+									setColumnOrder(colOrder);
+
+									// console.log(
+									//   "onDragUpdate",
+									//   dragUpdateObj.destination.index,
+									//   dragUpdateObj.source.index
+									// );
+									// console.log(temp);
+								}
+							}}
+						>
+							<Droppable droppableId="droppable" direction="horizontal">
+								{(droppableProvided, snapshot) => (
+									<div
+										{...headerGroup.getHeaderGroupProps()}
+										ref={droppableProvided.innerRef}
+										className="row header-group"
+									>
+										{headerGroup.headers.map((column, index) => (
+											<Draggable
+												key={column.id}
+												draggableId={column.id}
+												index={index}
+												isDragDisabled={!column.accessor}
+											>
+												{(provided, snapshot) => {
+													// console.log(column.getHeaderProps());
+
+													// const {
+													//   style,
+													//   ...extraProps
+													// } = column.getHeaderProps();
+
+													// console.log(style, extraProps);
+
+													return (
+														<div {...column.getHeaderProps()} className="cell header">
+															<div
+																{...provided.draggableProps}
+																{...provided.dragHandleProps}
+																// {...extraProps}
+																ref={provided.innerRef}
+																style={{
+																	...getItemStyle(snapshot, provided.draggableProps.style),
+																	// ...style
+																}}
+															>
+																{column.render('Header')}
+															</div>
+														</div>
+													);
+												}}
+											</Draggable>
+										))}
+										{/* {droppableProvided.placeholder} */}
+									</div>
+								)}
+							</Droppable>
+						</DragDropContext>
+					))}
+				</div>
+
+				<div className="rows" {...getTableBodyProps()}>
+					{rows.map(
+						(row, i) =>
+							prepareRow(row) || (
+								<div {...row.getRowProps()} className="row body">
+									{row.cells.map((cell) => {
+										return (
+											<div {...cell.getCellProps()} className="cell">
+												{cell.render('Cell')}
+											</div>
+										);
+									})}
+								</div>
+							)
+					)}
+				</div>
+			</div>
+			<div>
+				<div>
+					<IndeterminateCheckbox {...getToggleHideAllColumnsProps()} /> Toggle All
+				</div>
+				{allColumns.map((column) => (
+					<div key={column.id}>
+						<label>
+							<input type="checkbox" {...column.getToggleHiddenProps()} /> {column.Header}
+						</label>
+					</div>
+				))}
+				<br />
+			</div>
 			<table {...getTableProps()}>
 				<thead>
 					{headerGroups.map((headerGroup) => (
@@ -230,13 +458,19 @@ function Table({ columns: userColumns, data }) {
 							{headerGroup.headers.map((column) => (
 								// Add the sorting props to control sorting.
 								<th {...column.getHeaderProps()}>
+									{column.render('Header')}
 									<div {...column.getSortByToggleProps()}>
-										{column.render('Header')}
 										{/* Add a sort direction indicator */}
-										<span>{column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}</span>
+										{column.canSort ? (
+											<span>{column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ' ⏹️'}</span>
+										) : null}
 									</div>
 									{/* Render the columns filter UI */}
 									<div>{column.canFilter ? column.render('Filter') : null}</div>
+									<div
+										{...column.getResizerProps()}
+										className={`resizer ${column.isResizing ? 'isResizing' : ''}`}
+									/>
 								</th>
 							))}
 						</tr>
